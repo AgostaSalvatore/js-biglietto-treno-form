d761efe20ea15883d01962d2ed6b212d86a59cd4
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-26 14:29:18 +0100

    [UPD]Rename input field ID for name and add initial JavaScript structure

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//milestone 1
+
+
+
+
+
+
+
+
+
+
+
+
+//milestone 2
+//vado a recuperare tutti in camdpi della form e la form stessa
+const buttons1 = document.querySelectorAll('button1');
+const button2 = document.querySelectorAll('button2');
+const form = document.querySelector('form');
+const nameField = document.getElementById('name-fied');
+const ageField = document.getElementById('age-field');
+const rangeField = document.getElementById('range-field');
