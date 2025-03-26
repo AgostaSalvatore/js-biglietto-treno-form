5800b58f8c45b0d5c90dbf708d2f4c21f480da7a
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-26 14:42:21 +0100

    [UPD]Fix DOM element retrieval for buttons and enhance click event functionality

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@
 //milestone 2
 //RECUPERO TUTTI GLI ELEMENTIO DEL DOM CHE MI SERVONO
 //vado a recuperare tutti in camdpi della form e la form stessa
-const button1 = document.querySelectorAll('button1');
-const button2 = document.querySelectorAll('button2');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
 const form = document.querySelector('form');
 const nameField = document.getElementById('name-fied');
 const ageField = document.getElementById('age-field');
@@ -30,5 +30,8 @@ const costo = document.getElementById('costo');
 //creo azione al click del bottone
 button1.addEventListener('click', function(event){
     event.preventDefault(); //blocco il comportamento di default del form
+    carrozza.innerHTML = Math.floor(Math.random() * 9) + 1; //genero un numero random per la carrozza
+    codiceCP.innerHTML = Math.floor(Math.random() * 10000) + 1; //genero un numero random per il codice cp
+    //calcolo il costo del biglietto
     
 });
