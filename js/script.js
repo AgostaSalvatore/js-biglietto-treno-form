d630173386b532fcaa26ebeddc00e051d22bf41c
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-26 14:32:29 +0100

    [UPD]Add DOM element retrieval and button click event for ticket form

--- js/script.js
+++ js/script.js
@@ -12,10 +12,23 @@
 
 
 //milestone 2
+//RECUPERO TUTTI GLI ELEMENTIO DEL DOM CHE MI SERVONO
 //vado a recuperare tutti in camdpi della form e la form stessa
-const buttons1 = document.querySelectorAll('button1');
+const button1 = document.querySelectorAll('button1');
 const button2 = document.querySelectorAll('button2');
 const form = document.querySelector('form');
 const nameField = document.getElementById('name-fied');
 const ageField = document.getElementById('age-field');
 const rangeField = document.getElementById('range-field');
+
+//vado a recuperarre tutti i campi della card
+const offerta = document.getElementById('offerta');
+const carrozza = document.getElementById('carrozza');
+const codiceCP = document.getElementById('codice');
+const costo = document.getElementById('costo');
+
+//creo azione al click del bottone
+button1.addEventListener('click', function(event){
+    event.preventDefault(); //blocco il comportamento di default del form
+    
+});
